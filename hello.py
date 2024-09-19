3feb603617bbc4be0245ca4ed05e03b9d3e54d86
St80ene <etienejames5@gmail.com>
2024-09-19 16:24:15 +0100

    feat: start learning python
    Signed-off-by: St80ene <etienejames5@gmail.com>

---
+++ hello.py
@@ -0,0 +1,15 @@
+print('hello world!')
+
+print(5 * 5)
+
+if 5 > 2:
+  print('this is greater than 2')
+  print('this is less than 5')
+  
+  
+x = 5 # A VARIABLE
+y = "Hello there" # another variable
+
+
+print(x)
+print(y)
