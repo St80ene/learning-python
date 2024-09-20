2ef126501f9af1dafd1ca0e37ab3b441d390c559
St80ene <etienejames5@gmail.com>
2024-09-20 16:22:37 +0100

    learning(python): comment out code, cast variable to types
    Signed-off-by: St80ene <etienejames5@gmail.com>

--- hello.py
+++ hello.py
@@ -6,10 +6,20 @@ if 5 > 2:
   print('this is greater than 2')
   print('this is less than 5')
   
-  
+# Define a variable
 x = 5 # A VARIABLE
 y = "Hello there" # another variable
 
+# Comment out code
+# print(x)
 
-print(x)
+# cast variable to a data type
+x = str('Example')
+y = float(123)
+z = int(123)
+
+# know variable type
+print(type(x))
+print(type(y))
+print(type(z))
 print(y)
